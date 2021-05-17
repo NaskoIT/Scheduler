66ebc902236b0c53f6aa711cebcfd33b079d8de6
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-05-17 20:37:24 +0300

    Impelement the Login page using material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react"
 import { Route, Switch } from "react-router-dom"
 import Home from './pages/home/home.jsx'
-import Login from './pages/login/login.jsx'
-import Register from './pages/register/register.jsx'
+import SignIn from './pages/login/login.jsx'
+import SignUp from './pages/register/register.jsx'
 import NotFound from './pages/notFound/notFound.jsx'
 import Header from './components/Header'
 import './App.css'
@@ -13,8 +13,9 @@ export default function App() {
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/register" component={SignUp
+      } />
         <Route component={NotFound} />
       </Switch>
     </div>
--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,53 +1,106 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import './login.css'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-        this.state = {
-            email: '',
-            password: ''
-        }
+export default function SignIn() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-        this.onChangeHandler = this.onChangeHandler.bind(this);
-        this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const body = {
+      email,
+      password
+    };
 
-    onChangeHandler(e) {
-        this.setState({ [e.target.name]: e.target.value });
-    }
+    console.log(body);
+  }
 
-    onSubmitHandler(e) {
-        e.preventDefault();
-        const { email, password } = this.state;
-        // this.props.registerHandler({ username, password, email });
-        console.log(email);
-        console.log(password);
-    }
-
-    render() {
-        return (
-            <div class="w-50 mx-auto">
-                <h1 class="mb-4">Login</h1>
-                <form onSubmit={this.onSubmitHandler}>
-                    <div class="mb-3">
-                        <label htmlFor="email" class="form-label">Email address</label>
-                        <input onChange={this.onChangeHandler} name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" />
-                    </div>
-                    <div class="mb-3">
-                        <label htmlFor="password" class="form-label">Password</label>
-                        <input onChange={this.onChangeHandler} name="password" type="password" class="form-control" id="password" />
-                    </div>
-                    <button type="submit" class="btn btn-primary">Register</button>
-                    <Button variant="contained" color="primary">
-                        Hello World
-                    </Button>
-                </form>
-            </div>
-        )
-    }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h4">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={onSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link to="/register" variant="body2">
+              Already don't have an account? Sign up
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 }
-
-export default Login
