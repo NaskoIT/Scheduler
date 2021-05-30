3b7b5bc176be9fea86f3165a4298a1811fd96a9e
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-05-30 13:44:39 +0300

    Create new Component Hairdresser card and simplify the all hairdressers page component

--- client/src/pages/hairdressers/all.jsx
+++ client/src/pages/hairdressers/all.jsx
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import HairdresserCard from '../../components/HairdresserCard'
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
         marginTop: 25
-    },
-    card: {
-        height: 250,
-        width: 350,
-        textAlign: 'left',
     },
     control: {
         padding: theme.spacing(2),
@@ -33,35 +23,7 @@ export default function Hairdressers() {
                 <Grid container justify="center" spacing={5}>
                     {hairdressers.map(hairdresser => (
                         <Grid key={hairdresser.id} item>
-                            <Card className={classes.card}>
-                                <CardActionArea>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {`${hairdresser.username} - ${hairdresser.firstName} ${hairdresser.lastName}`}
-                                        </Typography>
-                                        <Typography variant="body2" component="p">
-                                            Email: {hairdresser.email}
-                                        </Typography>
-                                        <Typography variant="body2" component="p">
-                                            Phone: {hairdresser.phone}
-                                        </Typography>
-                                        <Typography variant="body2" component="p">
-                                            Location: {hairdresser.location}
-                                        </Typography>
-                                        <Typography variant="body2" component="p">
-                                            Work hours: {hairdresser.workHours.start} - {hairdresser.workHours.to}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            {hairdresser.description}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                    <Button size="small" color="primary">
-                                        Book
-                                    </Button>
-                                </CardActions>
-                            </Card>
+                            <HairdresserCard hairdresser={hairdresser} />
                         </Grid>
                     ))}
                 </Grid>
