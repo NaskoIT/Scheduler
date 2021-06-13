afef1ace1b86e70ac21f39481e7143b72134e5ea
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-06-13 17:01:41 +0300

    Get all the hairdressers

--- client/src/pages/hairdressers/all.jsx
+++ client/src/pages/hairdressers/all.jsx
@@ -15,13 +15,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Hairdressers() {
-    const [hairdressers, setHairdressers] = useState(hairdressersData);
+    const [hairdressers, setHairdressers] = useState();
     const classes = useStyles();
 
     useEffect(() => {
         getHairdressers()
             .then(response => {
-                setHairdressers(response);
+                setHairdressers(response.hairdressers)
             });
     });
 
@@ -39,34 +39,3 @@ export default function Hairdressers() {
         </Grid>
     )
 }
-
-const hairdressersData = [
-    {
-        "id": "1",
-        "username": "naskoIT",
-        "email": "nasko01_vasilev@abv.bg",
-        "firstName": "Atanas",
-        "lastName": "Vasilev",
-        "location": "Studentski grad, street 2, blok 18",
-        "phone": "0871234567",
-        "workHours": {
-            "start": "9:00",
-            "end": "18:00"
-        },
-        "description": "some description about the hairdresser"
-    },
-    {
-        "id": "2",
-        "username": "naskoIT2",
-        "email": "nasko0122_vasilev@abv.bg",
-        "firstName": "Vasi",
-        "lastName": "Andreev",
-        "location": "Studentski grad, street 25, blok 18",
-        "phone": "0871784567",
-        "workHours": {
-            "start": "10:00",
-            "end": "20:00"
-        },
-        "description": "some description about the hairdresser about the second hairdresser"
-    }
-]
--- client/src/services/hairdressersService.js
+++ client/src/services/hairdressersService.js
@@ -2,11 +2,45 @@ import { get, post } from './requester.js';
 import {apiRoutes} from '../constants/apiRoutes.js';
 
 export function getHairdressers() {
-    console.log('get hairdressers')
     return get(apiRoutes.hairdressers.all);
 }
 
 export function register(body) {
-    console.log('register hairdresser');
     return post(apiRoutes.hairdressers.register, body);
 }
+
+export function getHairdresserAppointments() {
+    return get(apiRoutes.hairdressers.appointments);
+}
+
+// Mocked hairdressers data
+const hairdressersData = [
+    {
+        "id": "1",
+        "username": "naskoIT",
+        "email": "nasko01_vasilev@abv.bg",
+        "firstName": "Atanas",
+        "lastName": "Vasilev",
+        "location": "Studentski grad, street 2, blok 18",
+        "phone": "0871234567",
+        "workHours": {
+            "start": "9:00",
+            "end": "18:00"
+        },
+        "description": "some description about the hairdresser"
+    },
+    {
+        "id": "2",
+        "username": "naskoIT2",
+        "email": "nasko0122_vasilev@abv.bg",
+        "firstName": "Vasi",
+        "lastName": "Andreev",
+        "location": "Studentski grad, street 25, blok 18",
+        "phone": "0871784567",
+        "workHours": {
+            "start": "10:00",
+            "end": "20:00"
+        },
+        "description": "some description about the hairdresser about the second hairdresser"
+    }
+]
