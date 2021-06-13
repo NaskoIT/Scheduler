5b2c9af583af63c66545893b8d12281fee9b64bc
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-06-13 11:30:14 +0300

    Register hairdressers api integration

--- client/src/services/hairdressersService.js
+++ client/src/services/hairdressersService.js
@@ -1,7 +1,12 @@
-import { get } from './requester.js';
+import { get, post } from './requester.js';
 import {apiRoutes} from '../constants/apiRoutes.js';
 
 export function getHairdressers() {
     console.log('get hairdressers')
     return get(apiRoutes.hairdressers.all);
+}
+
+export function register(body) {
+    console.log('register hairdresser');
+    return post(apiRoutes.hairdressers.register, body);
 }
