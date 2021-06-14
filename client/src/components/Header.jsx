856c5a292645d7e9a64a9ea949ca901af0914e25
Atanas Vasilev <nasko01vasilev@gmail.com>
2021-06-14 03:22:46 +0300

    Styling and clearing warning

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,6 @@ import { useHistory } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom'
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
